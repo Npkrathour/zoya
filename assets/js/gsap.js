d08fd019e426cd50594597e62722bb98466ac95b
Pinku rathour <pinkurathour2016@gmail.com>
2024-09-16 16:34:35 +0530

    add new code

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -9,61 +9,30 @@ gsap.ticker.add((time) => {
   lenis.raf(time * 1000);
 });
 
-// Reveal animation for text
-document.querySelectorAll(".reveal").forEach((text) => {
-  // Split text into words
-  let splitText = new SplitType(text, {
-    type: "words",
-  });
-  const section = text.closest(".split-text");
-
-  gsap.from(splitText.words, {
-    opacity: 0.1,
-    ease: "none",
-    stagger: 0.2,
-    duration: 2,
-    scrollTrigger: {
-      trigger: section,
-      start: "10% 50%",
-      end: "100% 50%",
-      scrub: true,
-    },
-  });
+gsap.to(".zoya-details-section", {
+  scrollTrigger: {
+    trigger: ".top-banner",
+    start: "top 10%",
+    end: "bottom 70%",
+    scrub: 2,
+  },
+  duration: 1,
+  height: "5vh",
+});
+// ==========================Overlay scroll Trigger==========
+gsap.set(".overlay", {
+  width: "100%",
+  opacity: 1,
 });
 
-// ===========================
-document.querySelectorAll(".text-split-container").forEach((text) => {
-  // Split text into words
-  let splitText = new SplitType(text, {
-    type: "words",
-  });
-  const section = text.closest(".text-container-split");
-
-  gsap.from(splitText.words, {
-    opacity: 0.1,
-    ease: "none",
-    stagger: 0.2,
-    duration: 2,
-    scrollTrigger: {
-      trigger: section,
-      start: "10% 80%",
-      end: "100% 50%",
-      scrub: true,
-    },
-  });
-});
-
-gsap.from(".overlay", {
+gsap.to(".overlay", {
   width: "0%",
   opacity: 0,
   duration: 1,
-  stagger: 0.2,
   scrollTrigger: {
     trigger: ".zoya-products",
-    start: "10% 50%",
-    end: "10% 50%",
-    toggleActions: "play none none reverse",
+    start: "10% 95%",
+    markers: true,
+    scrub: 2,
   },
-  width: "100%",
-  opacity: 1,
 });
